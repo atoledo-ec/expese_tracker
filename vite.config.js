6d311abeebc133f70e3de64a57fc4ed86a53ab20
Alejandro Toledo <atoledofr@gmail.com>
2025-11-21 21:31:09 -0500

    change config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This MUST match your repository name exactly (including the typo 'expese')
-  base: "/expense_tracker/",
+  base: './', // Use relative paths so it works when opening HTML directly
 })
